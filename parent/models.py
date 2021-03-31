011583fe5be20a206f2e8d5f00272d6f8e984e5a
safwanvk <safwanvalakundil@gmail.com>
2021-03-31 17:21:43 +0530

    :sparkles: Setup table Parent

--- parent/models.py
+++ parent/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Parent(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    name = models.CharField(max_length=64)
+    contact = models.IntegerField()
+    password = models.CharField(max_length=128)
+    email = models.CharField(max_length=64)
+    home_gps = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "Parent"
