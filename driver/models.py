a2014059fb8b082fabe5fbe0c7dc286121b3c349
safwanvk <safwanvalakundil@gmail.com>
2021-03-31 17:12:54 +0530

    :sparkles: Setup table Driver

--- driver/models.py
+++ driver/models.py
@@ -10,3 +10,16 @@ class Bus(models.Model):
 
     class Meta:
         db_table = "Bus"
+
+class Driver(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    name = models.CharField(max_length=64)
+    contact = models.IntegerField()
+    password = models.CharField(max_length=128)
+    bus_id = models.ForeignKey(Bus,to_field='id',on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "Driver"
