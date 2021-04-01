a227d744e00c2d3f38b74aad609bc4d52b81a6cc
safwanvk <safwanvalakundil@gmail.com>
2021-04-01 15:53:49 +0530

    :sparkles: Setup table User Type

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserType(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    role_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.role_name
+
+    class Meta:
+        db_table = "User_Type"
--- driver/views.py
+++ driver/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser 
+
 # Create your views here.
+@api_view(['POST'])
+def create_driver(request, *args, **kwargs):
+
+    data = JSONParser().parse(request)
+    print(data)
+
+    user_id = data.get('user_id')
+    bus_id = data.get('bus')
+    name = data.get('name')
+    contact = data.get('contact')
+    password = data.get('password')
+
+    if not (user_id and bus_id and name and contact and password):
+        return Response({"message": "Parameters missing"}, status=400)
+
+    try:
+
+        # reg = Attendance.objects.create(
+        #             sid=Student.objects.get(sid=std_id), 
+        #             arrival=arrival,
+        #             event_id=Event.objects.get(event_id=event_uid),
+        #     )
+
+        return Response({"message": "Successfully signed in"} ,status=200)
