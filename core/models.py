59a3c98a5330f6cbf9bd0050a998f7e24ad8e7b9
safwanvk <safwanvalakundil@gmail.com>
2021-04-01 16:07:38 +0530

    :sparkles: Setup table User

--- core/models.py
+++ core/models.py
@@ -10,3 +10,21 @@ class UserType(models.Model):
 
     class Meta:
         db_table = "User_Type"
+
+class User(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    address = models.CharField(max_length=500)
+    is_active = models.BooleanField()
+    role_id = models.ForeignKey(UserType,to_field='id',on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+    class Meta:
+        db_table = "User"
